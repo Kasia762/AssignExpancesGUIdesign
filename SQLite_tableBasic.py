8815bc72d9909165f070e712b988fac81629a56e
Kasia R <kasiarongiers2001@gmail.com>
2021-04-25 22:02:36 +0300

    lotto review

--- SQLite_tableBasic.py
+++ SQLite_tableBasic.py
@@ -2,13 +2,59 @@ import sqlite3
 import numpy as np
 import pandas as pd
 import matplotlib.pyplot as plt
+try:
+    import Tkinter as TK
+except ImportError:
+    import tkinter as TK
+
+def makeWindow(window):
+    global nameVar, phoneVar, name2Var, select
+    
+
+    frame1 = TK.Frame(window)
+    frame1.pack()
+
+    
+    TK.Label(frame1, text="date", bg="pink").grid(row=0, column=0, sticky=TK.W)
+    nameVar = TK.StringVar()
+    name = TK.Entry(frame1, textvariable=nameVar)
+    name.grid(row=0, column=1, sticky=TK.W, pady=10, ipadx=50)
+  
+    TK.Label(frame1, text="amount", bg="pink").grid(row=1, column=0, sticky=TK.W)
+    phoneVar= TK.StringVar()
+    phone= TK.Entry(frame1, textvariable=phoneVar)
+    phone.grid(row=1, column=1, sticky=TK.W, pady=10, ipadx=50)
+    
+    TK.Label(frame1, text="cat", bg="pink").grid(row=2, column=0, sticky=TK.W)
+    name2Var = TK.StringVar()
+    name2 = TK.Entry(frame1, textvariable=name2Var)
+    name2.grid(row=2, column=1, sticky=TK.W, pady=10, ipadx=50)
+  
+    frame2 = TK.Frame(window)       # Row of buttons
+    frame2.pack()
+    b1 = TK.Button(frame2, text="Add", command=addEntry)
+    #b2 = TK.Button(frame2, text="Update", command=updateEntry)
+    #b3 = TK.Button(frame2, text="Delete", command=deleteEntry)
+    #b4 = TK.Button(frame2, text="Load", command=loadEntry)
+    b1.pack(side=TK.LEFT,ipadx=10, padx=5)
+    #b2.pack(side=TK.LEFT,ipadx=10, padx=5)
+    #b4.pack(side=TK.LEFT,ipadx=10, padx=5)
+    #b3.pack(side=TK.LEFT,ipadx=10, padx=30)
+
+    frame3 = TK.Frame(window)       # select of names
+    frame3.pack(padx=5, pady=5)
+    scroll = TK.Scrollbar(frame3, orient=TK.VERTICAL)
+    select = TK.Listbox(frame3, yscrollcommand=scroll.set, height=8, width=50)
+    scroll.config (command=select.yview)
+    scroll.pack(side=TK.RIGHT, fill=TK.Y)
+    select.pack(side=TK.LEFT,  fill=TK.BOTH, expand=1)
+    return win
 
 
-#date = "23-10-2020"
-#shop ='Hesburger'
-#price = 15.5
-#cat = "restaurant"
-val =[("12-03-2019","18.34","food"),
+val = [([nameVar.get(),phoneVar.get(),name2Var.get()])]
+
+
+'''val =[("12-03-2019","18.34","food"),
       ("07-03-2019","1.34","restaurant"),
       ("12-04-2019","10.65","food"),
       ("12-03-2019","1.34","home"),
@@ -17,7 +63,7 @@ val =[("12-03-2019","18.34","food"),
       ("10-05-2019","160.65","bills"),
       ("12-03-2020","12.68","bills"),
       ("15-09-2019","672.21","bills"),
-      ]
+      ]'''
 
 conn = sqlite3.connect('spend.db')
 cur = conn.cursor()
@@ -33,7 +79,7 @@ def create_table():
     conn.commit()
 
 
-def insert_data():
+def addEntry():
     sql = '''INSERT INTO spendings 
     (DATE, PRICE, CAT) 
     values (?,?,?)'''
@@ -53,8 +99,16 @@ def select_data():
     conn.commit()
   
 create_table()
-insert_data()
+
 select_data()     
 conn.close()
 
+win = TK.Tk()
 
+win = makeWindow(win)
+
+
+win.configure(bg="pink")
+win.title("My Phone Book")
+win.geometry("500x300")
+win.mainloop()
